da4b0a6ab7ae5d3aaa666a5d6313befccf8a6dca
xubuntu <kamil.borkow@wp.pl>
2024-07-29 19:28:36 +0200

    finished day 1

--- src/bootcamp_chat_frontend/src/App.vue
+++ src/bootcamp_chat_frontend/src/App.vue
@@ -1,21 +1,37 @@
 <script lang="ts">
 import { ref } from 'vue';
-import { bootcamp_chat_backend } from 'declarations/bootcamp_chat_backend/index';
+import { bootcamp_chat_backend } from '../../declarations/bootcamp_chat_backend';
+import { AuthClient } from '@dfinity/auth-client';
+import { HttpAgent } from '@dfinity/agent';
+import type { Identity } from '@dfinity/agent';
 
-export default{
-  data(){
-    return{
-      newNote: "", 
-      notes: [] as string[]   
+export default {
+  data() {
+    return {
+      newNote: "",
+      notes: [] as string[],
+      identity: undefined as undefined | Identity,
     }
   },
-  methods:{
-   async dodajNotatke(){
+  methods: {
+    async dodajNotatke() {
       await bootcamp_chat_backend.add_note(this.newNote)
-      await this.pobierzNotatki
+      await this.pobierzNotatki()
     },
-    async pobierzNotatki(){
-      this.notes = await bootcamp_chat_backend.get_nodes()
+    async pobierzNotatki() {
+      this.notes = await bootcamp_chat_backend.get_notes()
+    },
+    async login() {
+      const authClient = await AuthClient.create();
+      await authClient.login({
+        identityProvider: "http://b77ix-eeaaa-aaaaa-qaada-cai.localhost:4943/"
+      })
+
+      const identity = authClient.getIdentity();
+      console.log("Zalogowano", identity.getPrincipal())
+      this.identity = identity;
+      
+     //const agent = new HttpAgent({ identity });
     }
   },
   mounted(){
@@ -29,12 +45,12 @@ export default{
     <img src="/logo2.svg" alt="DFINITY logo" />
     <br />
     <br />
+    {{ identity?.getPrincipal() }} <button @click="login">login</button>
     <div>
-      {{ notes }}  
+      {{ notes }}
     </div>
     <div>
-      <textarea v-model="newNote"></textarea> <button @click = "dodajNotatke">Dodaj notatke</button>
+      <textarea v-model="newNote"></textarea><button @click="dodajNotatke">Dodaj notatke</button>
     </div>
-    
   </main>
 </template>
